97ef0c9a86a2bf38edb9d803cee9ea693fca97cc
Aashka Pallath <77869228+pallathaa@users.noreply.github.com>
2025-07-13 16:57:14 -0700

    leetcode 572, 637, 347

--- medium/347_TopK_Frequent_Elements.py
+++ medium/347_TopK_Frequent_Elements.py
@@ -1,8 +1,6 @@
 '''
 Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
 
- 
-
 Example 1:
 
 Input: nums = [1,1,1,2,2,3], k = 2
@@ -23,5 +21,25 @@ It is guaranteed that the answer is unique.
 
 '''
 Notes: 
-
+- keep a dictionary where key is element and value is the number of times it appears nums
+- keep a frequency list where freq_list[i] is a list of elements that occur i times in nums
+    - use the dictionary to fill the frequency list
 '''
+
+def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count = dict()
+        freq_list = [[] for i in range(len(nums)+1)]
+        output = []
+
+        for n in nums:
+            count[n] = 1 + count.get(n, 0)
+        
+        for n, c in count.items():
+            freq_list[c].append(n)
+
+        for i in range(len(freq_list) - 1, 0, -1):
+            for n in freq_list[i]:
+                output.append(n)
+
+                if len(output) == k: 
+                    return output
